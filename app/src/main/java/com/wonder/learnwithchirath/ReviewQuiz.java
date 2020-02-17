4add17aec22cbf29d4c3aff727efadfc28484557
Yasiru Priyadarshana <hly.priyadarshana@gmail.com>
2020-02-17 16:50:38 +0530

    review question details

--- app/src/main/java/com/wonder/learnwithchirath/ReviewQuiz.java
+++ app/src/main/java/com/wonder/learnwithchirath/ReviewQuiz.java
@@ -6,6 +6,9 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,7 +16,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.wonder.learnwithchirath.Adpter.ListAdapterReview;
+import com.wonder.learnwithchirath.Adpter.RecyclerAdapterReview;
 import com.wonder.learnwithchirath.Object.Answer;
+import com.wonder.learnwithchirath.Object.Review;
 
 import java.util.ArrayList;
 
@@ -22,6 +27,9 @@ public class ReviewQuiz extends AppCompatActivity {
     private ArrayList<Answer> answers;
     private ListView ReviewListView;
     private ListAdapterReview adapter;
+    private RecyclerView ReviewAnsListView;
+    private RecyclerAdapterReview adapterReview;
+    private ArrayList<Review> reviews;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,31 +37,53 @@ public class ReviewQuiz extends AppCompatActivity {
         databaseReference = FirebaseDatabase.getInstance().getReference("Answer_Student");
         ReviewListView=(ListView)findViewById(R.id.recyclerviewreview);
         answers=new ArrayList<>();
+        reviews=new ArrayList<>();
         viewAllFiles();
+
     }
+
+    private void viewAllFiles2() {
+        ReviewAnsListView=(RecyclerView)findViewById(R.id.recyclerreviewlist);
+        LinearLayoutManager layoutManager= new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.HORIZONTAL,false);
+        ReviewAnsListView.setLayoutManager(layoutManager);
+        ReviewAnsListView.setLayoutManager(new GridLayoutManager(ReviewQuiz.this,3));
+        adapterReview=new RecyclerAdapterReview(reviews);
+        ReviewAnsListView.setAdapter(adapterReview);
+    }
+
     private void viewAllFiles() {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                int i=1;
+                double correct=0;
+
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     Answer answer = postSnapshot.getValue(Answer.class);
                     answers.add(answer);
-
+                    if (answer.getAnswer().get(i)>0){
+                        correct++;
+                    }
+                    Review review=new Review(i,(correct/3.0)*100.0);
+                    i++;
+                    reviews.add(review);
                 }
+
+
+
 
 
                 adapter = new ListAdapterReview(ReviewQuiz.this, R.layout.itemreview,answers);
                 ReviewListView.setAdapter(adapter);
-
+                viewAllFiles2();
             }
-
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
-
-
     }
+
+
 }
